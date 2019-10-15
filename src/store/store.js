d90da996f8e675d59b3bd0d014575786f0cb1bf2
Dave Regg <drregg6@gmail.com>
2019-10-15 19:48:56 -0400

    library now persists with vuex-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
 
 import library from "./modules/library";
 
 Vue.use(Vuex);
+const vuexPersist = new VuexPersist({
+  key: 'vuex-library',
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
+  plugins: [vuexPersist.plugin],
   modules: {
     library
   }
